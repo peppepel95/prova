b717376640575b00c9fbcb37cff24f903368bdf4
Giuseppe Pellegrino <peppe95.pellegrino@gmail.com>
2017-12-16 17:35:45 +0100

    controlli

--- Esercizi/EditDistance.py
+++ Esercizi/EditDistance.py
@@ -5,9 +5,9 @@ def editDistance (s1, s2):
 
     M = [[0] * (m+1) for k in range(n+1)] #matrice n+1 x m+1
 
-    for k in range(1,n+1):
+    for k in range(1,n+1): #s1
         M[k][0] = k
-    for k in range(1,m+1):
+    for k in range(1,m+1): #s2
         M[0][k] = k
 
     for i in range(n):
@@ -20,6 +20,31 @@ def editDistance (s1, s2):
 
     return M
 
+def editTranscript(s1, s2, M): # j ^ k <- righe colonne
+    solution = []
+    j,k = len(s1), len(s2)
+
+    while M[j][k] > 0:
+        temp = min(M[j-1][k-1],M[j-1][k],M[j][k-1])
+
+        if temp == M[j-1][k]:
+            solution.append("D")
+            j -= 1
+        elif temp == M[j][k-1]:
+            solution.append("I")
+            k -= 1
+        elif temp == M[j-1][k-1]:
+            if temp == M[j][k]:
+                solution.append("M")
+            j -= 1
+            k -= 1
+        else:
+            solution.append("R")
+            j -= 1
+            k -= 1
+
+    return "".join(reversed(solution))
+
 def printMatriceED(M):
     for i in range(len(M) - 1):
         print(M[i])
@@ -31,4 +56,6 @@ def printMatriceED(M):
             print(M[-1][j] , ",", end=" ", sep="")
     print("]\n")
 
-printMatriceED(editDistance("casa","capa"))
+M = editDistance("winter","writers")
+printMatriceED(M)
+print(editTranscript("winter","writers",M))
