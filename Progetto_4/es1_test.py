5af177e6ee4dd0559f1d70a6a13133d0315b88e2
Giuseppe Pellegrino <peppe95.pellegrino@gmail.com>
2018-01-06 16:13:14 +0100

    final

--- Progetto_4/es1_test.py
+++ Progetto_4/es1_test.py
@@ -5,7 +5,7 @@ import pstats
 for i in range(50):
 
     m = 250 + i*15
-    print('\033[1;31m_____________________________G\033[1;m',"(50,", m, ")")
+    print('\033[1;31m_____________________________G\033[1;m',"( 50 ,", m, ")")
 
     print('\033[1;31m_____________________________min_vertex_cover\033[1;m')
     G = generateGraph(50, m)
@@ -20,8 +20,6 @@ for i in range(50):
     print(len(vertex_cover))
 
     print('\033[1;31m_____________________________greedy_vertex_cover\033[1;m')
-
-    print("Random graph generated")
 
     vertex_cover = []
     cProfile.run("vertex_cover = G.greedy_vertex_cover()", "test")
